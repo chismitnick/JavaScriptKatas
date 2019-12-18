68223fd0970e042c0006eebc9777ddd6b2f6ad42
csamiselo <csamiselo@github.com>
2019-12-18 15:46:49 +0200

    First javascript program written in Javascript following tutorials from freecodecamp.org

---
+++ first.js
@@ -0,0 +1,21 @@
+// A simple program to introduce the basic cooncepts of Javascript programming
+// csamiselo@github.com December 2019
+
+var number1 = 10;
+var number2 = 20;
+
+var sum;
+var difference;
+var product;
+var quotient;
+
+sum = number1 + number2;
+difference = number1 - number2;
+product = number1 * number2;
+quotient = number1 / number2;
+
+console.log("The results of the mathematical operations are :");
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient)
