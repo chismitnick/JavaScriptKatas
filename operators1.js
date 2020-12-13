f42ccb958fc56d30cd4e7a49d33d347b661180de
chismitnick <chisambwe.samiselo@gmail.com>
2020-12-13 17:41:48 +0200

    Progress

---
+++ operators1.js
@@ -0,0 +1,9 @@
+let x = 3
+var y = 12
+const z = 5.9
+let b = 2.89e6
+console.log("The data type of the variable x is", typeof(x))
+console.log(`Multiplication of y and constant z : ${y * z}`)
+
+console.log("The value of the exponential variable x :" + b)
+
